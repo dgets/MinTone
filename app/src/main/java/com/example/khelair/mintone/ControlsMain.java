a9fad95cac5329e27ab09c1a2c33dbcd2faab4d8
Damon Getsman <damo.gets@gmail.com>
2017-09-27 06:14:22 -0500

    Closed #1 (play button toggle display), also added strings.xml to the vcs files.

--- app/src/main/java/com/example/khelair/mintone/ControlsMain.java
+++ app/src/main/java/com/example/khelair/mintone/ControlsMain.java
@@ -33,9 +33,9 @@ public class ControlsMain extends AppCompatActivity {
     private boolean regen       =   true;
 
     //private RadioGroup rgrp;
-    private RadioButton btnOne, btnTwo, btnThree, btnFour;
+    private RadioButton btnOne, btnTwo, btnThree;   //, btnFour;
     private SeekBar sb;
-    private Button togglePlayback, setManually;
+    private Button btnTogglePlayback, btnSetManually;
     private EditText manualFreqValue;
 
     AudioTrack ouahful;
@@ -51,8 +51,9 @@ public class ControlsMain extends AppCompatActivity {
         btnThree = (RadioButton) findViewById(R.id.rbtThree);
         //btnFour = (RadioButton) findViewById(R.id.rbtFour);
 
-        togglePlayback = (Button) findViewById(R.id.btnTglPlaying);
-        setManually = (Button) findViewById(R.id.btnManualFreqChange);
+        btnTogglePlayback = (Button) findViewById(R.id.btnTglPlaying);
+        btnSetManually = (Button) findViewById(R.id.btnManualFreqChange);
+        btnSetManually.setClickable(false);
         manualFreqValue = (EditText) findViewById(R.id.edtManualFreq);
 
         sb = (SeekBar) findViewById(R.id.sbrFreqSlider);
@@ -76,42 +77,58 @@ public class ControlsMain extends AppCompatActivity {
         ouahful = new AudioTrack(AudioManager.STREAM_MUSIC,
                 sampleRate, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT,
                 (2 * numSamples), AudioTrack.MODE_STATIC);
-        ouahful.setNotificationMarkerPosition(8000);
-        ouahful.setPlaybackPositionUpdateListener(new AudioTrack.OnPlaybackPositionUpdateListener()
-        {
-            @Override
-            public void onPeriodicNotification(AudioTrack track) {
-                // nothing to do
-            }
-
-            @Override
-            public void onMarkerReached(AudioTrack track) {
-                //playing = !playing;
-                togglePlayback.callOnClick();
-            }
-        });
     }
 
+    /*
+     *  Widget controls
+     */
     public void onTogglePlay(View view) {
         byte    sounds[] = initSound(freq);
         /* AudioTrack  ouahful = new AudioTrack(AudioManager.STREAM_MUSIC,
                 sampleRate, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT,
                 sounds.length, AudioTrack.MODE_STATIC); */
 
+        //btnSetManually.setClickable(false);   //wut?
+
         if (!playing) {
             if (regen) {
                 sounds = initSound(freq);
             }
             ouahful = buildTrack(sounds);
+            ouahful.setNotificationMarkerPosition(8000);
+
+            //should this be handled here?  probably not, methinks
+            ouahful.setPlaybackPositionUpdateListener(
+             new AudioTrack.OnPlaybackPositionUpdateListener()
+              {
+                @Override
+                public void onPeriodicNotification(AudioTrack track) {
+                    // nothing to do
+                }
+
+                @Override
+                public void onMarkerReached(AudioTrack track) {
+                    if (playing) {
+                        ouahful.stop();
+                        btnTogglePlayback.setText("Play"); //working every other time?  wtf
+                        playing = !playing;
+                    }
+                }
+              });
+            btnTogglePlayback.setText("Pause");
             ouahful.play();
         } else {
+            btnTogglePlayback.setText("Play");
             ouahful.stop(); //need to convert to save the track beyond this method
         }
 
         playing = !playing;
     }
 
-    public void onChangeGrpFreqPresets(View view) {
+    public void onClickGrpFreqPresets(View view) {
+        btnSetManually.setClickable(false);
+
+        //use getCheckedRadioButtonId for a little less redundancy
         if (btnOne.isChecked()) {
             //so yeah, this is obviously a terrible way to do this
             freq = Integer.parseInt((String) btnOne.getText());
@@ -123,7 +140,6 @@ public class ControlsMain extends AppCompatActivity {
 
         sb.setProgress((int) freq);
         regen = true;
-        //return freq;
     }
 
     public void onSetManualFreq(View view) {
@@ -131,26 +147,17 @@ public class ControlsMain extends AppCompatActivity {
         freq = getManualFreq();
     }
 
-    public int getManualFreq() {
-        int ouah = 0;
-
-        try {
-            ouah = Integer.parseInt(manualFreqValue.getText().toString());
-        } catch (NumberFormatException e) {
-            //god ouah ouah ouah
-            ouah = 100;
-        }
-
-        //find out how to retrieve freq_min & _max from integers.xml
-        /* if ((ouah < 20) || (ouah > 20000)) {
-            //out of range, kill the user for the blasphemy
-
-            //throw MyException("Value out of range");
-        } */
-
-        return (int) ouah;
+    public void onClickManualFreqEntry(View view) {
+        btnSetManually.setClickable(true);
     }
 
+    public void onClickSeekbar(View view) {
+        btnSetManually.setClickable(false);
+    }
+
+    /*
+     * Methods mein
+     */
     private AudioTrack buildTrack(byte samples[]) {
         AudioTrack track = new AudioTrack(AudioManager.STREAM_MUSIC,
                 sampleRate, AudioFormat.CHANNEL_OUT_MONO,
@@ -179,5 +186,25 @@ public class ControlsMain extends AppCompatActivity {
 
         regen = false;
         return sampleInProgress;
+    }
+
+    public int getManualFreq() {
+        int ouah = 0;
+
+        try {
+            ouah = Integer.parseInt(manualFreqValue.getText().toString());
+        } catch (NumberFormatException e) {
+            //god ouah ouah ouah
+            ouah = 100;
+        }
+
+        //find out how to retrieve freq_min & _max from integers.xml
+        /* if ((ouah < 20) || (ouah > 20000)) {
+            //out of range, kill the user for the blasphemy
+
+            //throw MyException("Value out of range");
+        } */
+
+        return (int) ouah;
     }
 }
